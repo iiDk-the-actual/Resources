1394ab8b8ed2ca996d59df01e06eb4659cf3024f
iiDk <54154105+iiDk-the-actual@users.noreply.github.com>
2022-12-22 18:45:17 -0500

    Create Serverhop.lua

---
+++ Miscellaneous/Serverhop.lua
@@ -0,0 +1,17 @@
+function Serverhop()
+    game:GetService("TeleportService").TeleportInitFailed:Connect(function() 
+        Serverhop() 
+    end)
+	local x = {}
+	for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+		if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+			x[#x + 1] = v.id
+		end
+	end
+	if #x > 0 then
+		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+	else
+		game:GetService("TeleportService"):Teleport(game.PlaceId)
+	end
+end
+Serverhop()
